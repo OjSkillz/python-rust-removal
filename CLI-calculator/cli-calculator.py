d08300e74dc883ffacf139d7603c8082c581dd81
OjSkillz <soojo06@gmail.com>
2026-02-03 12:49:45 +0100

    "Errors from user input handled with appropriate responses "

--- CLI-calculator/cli-calculator.py
+++ CLI-calculator/cli-calculator.py
@@ -3,17 +3,21 @@
 # collect second number from user
 # return result of operation
 
+
 def calculate(first_number, operator, second_number):
+
     if operator == "+":
         return add(first_number, second_number)
     elif operator == '-':
         return subtract(first_number, second_number)
+
     elif operator in ('*', 'x', 'X'):
         return multiply(first_number, second_number)
     elif operator == '/':
         return divide(first_number, second_number)
 
-    return "Invalid Operator"
+    else:
+        return "Invalid Operator"
 
 def add(first, second):
     return first + second
@@ -30,13 +34,22 @@ def divide(first, second):
     return first / second
 
 
-try:
-    first_input = float(input("Enter first number: "))
-    operand = input("Enter operator ('+', '-', '* or x', and '/'): " )
-    second_input = float(input("Enter second number: "))
 
-    result = calculate(first_input, operand, second_input)
-    print(f"{first_input} {operand} {second_input} = {result}")
 
-except ValueError:
-    print("Please enter a valid number!\n")
+
+sentinel = "unfinished"
+while sentinel == "unfinished" :
+    try:
+        first_input = float(input("Enter first number: "))
+        operand = input("Enter operator ('+', '-', '* or x', and '/'): " )
+        second_input = float(input("Enter second number: "))
+
+        result = calculate(first_input, operand, second_input)
+        if result != "Invalid Operator":
+            print(f"{first_input} {operand} {second_input} = {result}")
+            sentinel = "done"
+        else: print(result)
+
+    except ValueError:
+        print("Please enter a valid number!\n")
+
