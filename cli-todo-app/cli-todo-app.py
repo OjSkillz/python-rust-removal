4cd4de691bf049938e7aa1d9c8f37a19a95ad157
OjSkillz <soojo06@gmail.com>
2026-02-03 17:54:28 +0100

    "Command Line prompts added with match cases"

--- cli-todo-app/cli-todo-app.py
+++ cli-todo-app/cli-todo-app.py
@@ -1,3 +1,5 @@
+from re import match
+
 task = {}
 tasks = []
 
@@ -15,4 +17,27 @@ def delete_task(title):
         for keys, values in task_.items():
             if values == title:
                 tasks.remove(task_)
+
+
+print("Welcome to your ToDo App\nPlease choose from the options below: ")
+choice = int(input("1. Add Task"
+                   "2. View Tasks"
+                   "3. Delete Task"))
+
+match choice:
+    case 1:
+        day = input("Enter day: ")
+        title = input("Enter title")
+        add_task(day, title)
+        print("Task has been added successfully")
+
+    case 2:
+        view_tasks()
+
+    case 3:
+        response = input("Enter the title of the task you would like to delete")
+        delete_task(response)
+        print("Task has been deleted successfully")
+
+
 
