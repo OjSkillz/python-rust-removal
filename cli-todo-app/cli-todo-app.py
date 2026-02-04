4b9965b38e6dadab2a5b40cd3e2e028c4662defd
OjSkillz <soojo06@gmail.com>
2026-02-04 12:13:37 +0100

    "Recursive calls added to main menu function"
    Signed-off-by: OjSkillz <soojo06@gmail.com>

--- cli-todo-app/cli-todo-app.py
+++ cli-todo-app/cli-todo-app.py
@@ -18,26 +18,37 @@ def delete_task(title):
             if values == title:
                 tasks.remove(task_)
 
+def main_menu():
+    choice = int(input("1.Add Task\n"
+                       "2.View Tasks\n"
+                       "3.Delete Task\n"
+                       "4.Exit\n"))
+
+    match choice:
+        case 1:
+            day = input("Enter day: ")
+            title = input("Enter title: ")
+            add_task(day, title)
+            print("Task has been added successfully")
+            main_menu()
+        case 2:
+            print(view_tasks())
+            main_menu()
+
+        case 3:
+            response = input("Enter the title of the task you would like to delete")
+            delete_task(response)
+            print("Task has been deleted successfully")
+            main_menu()
+
+        case 4:
+            print("App closed")
 
 print("Welcome to your ToDo App\nPlease choose from the options below: ")
-choice = int(input("1.Add Task\n"
-                   "2.View Tasks\n"
-                   "3.Delete Task\n"))
+main_menu()
 
-match choice:
-    case 1:
-        day = input("Enter day: ")
-        title = input("Enter title")
-        add_task(day, title)
-        print("Task has been added successfully")
 
-    case 2:
-        view_tasks()
 
-    case 3:
-        response = input("Enter the title of the task you would like to delete")
-        delete_task(response)
-        print("Task has been deleted successfully")
 
 
 
