98b617a6fa3081e6ecd00d9a5dd8ea202f1c0a10
OjSkillz <soojo06@gmail.com>
2026-02-07 15:15:54 +0100

    "function count_words updated"
    Signed-off-by: OjSkillz <soojo06@gmail.com>

--- word-counter-app/word-counter.py
+++ word-counter-app/word-counter.py
@@ -1,11 +1,16 @@
 def read_file(path):
-    ...
+    with open(path, "r") as file:
+        return file.read()
 
 def clean_text(text):
-    ...
+    text.replacce(["!",":",";",",",".","?"], " ").lower().split("")
+    return text
 
 def count_words(words):
-    ...
-
+    for word in words:
+        print(f"{word} : {words.count(word)}")
+        counter = {word: words.count(word)}
+        return counter
+    return
 def print_top_words(counter, limit=10):
     ...
