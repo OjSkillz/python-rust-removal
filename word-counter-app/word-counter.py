76f2a471323bfb4ddbe71556de299b80c9a60be3
OjSkillz <soojo06@gmail.com>
2026-02-08 23:57:54 +0100

    "function to clean text file refactored"
    Signed-off-by: OjSkillz <soojo06@gmail.com>

--- word-counter-app/word-counter.py
+++ word-counter-app/word-counter.py
@@ -1,10 +1,13 @@
+from click import help_option
+
+
 def read_file(path):
     with open(path, "r") as file:
         return file.read()
 
-def clean_text(text):
-    text.replacce(["!",":",";",",",".","?"], " ").lower().split(" ")
-    return text
+def clean_text(file):
+    cleaned_file = file.replace(["!",":",";",",",".","?"], " ").lower().split(" ")
+    return cleaned_file
 
 def count_words(words):
     for word in words:
@@ -17,3 +20,9 @@ def count_words(words):
 def print_top_words(counter, limit=10):
     for word, count in sorted(counter.items(), key=lambda item: item[1], reverse=True)[:limit]:
         print(f"{word}: {count}")
+
+file_path = input("Enter the file path: ")
+read_text = read_file(file_path)
+cleaned_text = clean_text(read_text)
+count_words(cleaned_text)
+print_top_words(count_words(clean_text(read_text)))
